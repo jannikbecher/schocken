1b624de453d601f7dad83091480fec5dbeb8ddbe
Jannik Becher <becher.jannik@gmail.com>
2019-03-14 16:37:52 +0100

    adding test to test the player module

--- test/game_player_test.exs
+++ test/game_player_test.exs
@@ -2,4 +2,42 @@ defmodule Schocken.Game.PlayerTest do
   use ExUnit.Case
 
   alias Schocken.Game.Player
+
+  setup do
+    player = Player.new("Jannik")
+    {:ok, player: player}
+  end
+
+  test "create new player", context do
+    player = context.player
+    result = %Player{
+      name: "Jannik",
+      current_toss: nil,
+      num_coaster: 0,
+      lost_half: false
+    }
+    assert player == Map.put(result, :current_toss, player.current_toss)
+  end
+
+  test "roll one dice", context do
+    player = context.player
+    choice = List.first(player.current_toss.dices)
+    new_player = Player.roll_dices(player, choice)
+    assert player != new_player
+    assert new_player.current_toss.tries == 2
+  end
+
+  test "roll all dices", context do
+    player = context.player
+    new_player = Player.roll_dices(player, :all)
+    assert player != new_player
+  end
+
+  test "roll two dices", context do
+    player = context.player
+    dices = player.current_toss.dices
+    choices = [List.first(dices), List.last(dices)]
+    new_player = Player.roll_dices(player, choices)
+    assert player != new_player
+  end
 end
